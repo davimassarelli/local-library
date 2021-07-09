f101dcc5cecb802fe32b1834e0f28ce8d4d750cc
Davi Massarelli <83038782+davimassarelli@users.noreply.github.com>
2021-07-09 11:20:16 -0300

    Implementação de paginas para alteração do autor, todas contendo campos de formulário e formatação padrão do django

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Book, Author, BookInstance, Genre
 from .forms import RenewBookForm
@@ -126,3 +127,19 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
